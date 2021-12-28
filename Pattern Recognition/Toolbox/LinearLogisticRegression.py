388bb36f7d37dfb0f862e81b65394c2081f858e7
ridhimagarg <garg.ridhima72@gmail.com>
2021-12-28 23:43:50 +0100

    linear regression pr

--- Pattern Recognition/Toolbox/LinearLogisticRegression.py
+++ Pattern Recognition/Toolbox/LinearLogisticRegression.py
@@ -33,11 +33,17 @@ Iteratively estimate the parameters of the decision function using the Newton me
 """
 
 import numpy as np
+import math
 
 
 
 class LinearLogisticRegression(object):
     def __init__(self, learningRate=0.5, maxIterations=100):
+
+        self.learningRate = 0.5
+        self.maxIterations = 100
+        # self.theta = np.random.normal()
+
         return None
 
     def fit(self, X, y):
@@ -45,13 +51,52 @@ class LinearLogisticRegression(object):
         Be careful when using the GUI classes can have different class numbers from 0/1.
         You need to map the to class number somehow to 0 and 1.
         """
-        return None
+
+
+        intitial_theta = np.zeros((2,1))
+        self.theta = intitial_theta
+
+        while self.maxIterations:
+
+            print("Train data shape", X.shape)
+            print("Test data shape", y.shape)
+
+            y_pred = [self.gFunc(x, self.theta) for x in X]
+
+            # print((2* np.multiply((y.reshape(1,10) - np.array(y_pred).reshape(1,10)),  np.array([self.derivative_sigmoid(x) for x in y_pred]).reshape(1,10))).shape)
+
+            # dertheta = ((2* (y- np.array(y_pred)), np.array([self.derivative_sigmoid(x) for x in np.array(y_pred)]).T).T* X).T
+
+            dertheta = (2* np.multiply((y.reshape(1,10) - np.array(y_pred).reshape(1,10)),  np.array([self.derivative_sigmoid(x) for x in y_pred]).reshape(1,10))) @ X
+
+            print("Derivative of weights shape", dertheta.shape)
+
+            self.theta = self.theta - self.learningRate*dertheta.reshape(2,1)
+
+            self.maxIterations -=1
+        
+        return self.theta
+
+        # return None
 
     def gFunc(self, X, theta):
-        return None
+
+        return 1/(1+ np.exp(-np.dot(X, theta)))
+
+    def sigmoid(self, inp):
+ 
+        return 1/(1+ np.exp(-inp))
+
+    def derivative_sigmoid(self, inp):
+
+        return self.sigmoid(inp) * (1- self.sigmoid(inp))
 
     def predict(self, X):
-        return None
+
+        print("Test data", X.shape)
+
+
+        return [1/(1+ np.exp(-np.dot(x, self.theta))) for x  in X]
 
 
 
@@ -88,7 +133,7 @@ def test_linear_logistic_regression():
     # During training we know the labels
     classifier.fit(sample_position_training, sample_labels_training)
 
-    # This is our true y
+    #This is our true y
     sample_labels_inference = np.random.randint(num_classes, size=(num_inference_samples,))
     # These are the positions of the training samples
     sample_position_inference = cluster_centers[sample_labels_inference] + \
--- Pattern Recognition/Toolbox/LinearRegression_Solution.py
+++ Pattern Recognition/Toolbox/LinearRegression_Solution.py
@@ -19,23 +19,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with the Python Classification Toolbox.
 #    If not, see <http://www.gnu.org/licenses/>.
-"""
-Implement Linear Regression in LinearRegression.py.
-The parameter 'classification' can be ignored, as well as the 'predict' function.
-The parameter lossFunction can either be 'l2' for linear regression or 'huber'
-for regression using a Huber loss function.
-In this case, lossFunctionParam is the parameter of the Huber function. The 'fit' function should
-compute the affine regression from X onto y (i.e. you need to extend the parameter vector with a constant).
-
-Hint: to compute the regression parameters for the Huber function,
-it may be easier use scipy.optimize.minimize on the pre-defined functions
-than to implement the full optimization yourself. For the L2-Loss,
-the regression can be computed using the pseudo-inverse.
-"""
 
 import numpy
-import scipy.optimize
 import numpy as np
+import scipy.optimize
 
 
 class LinearRegression(object):
@@ -43,93 +30,81 @@ class LinearRegression(object):
                  lossFunction='l2',
                  lossFunctionParam=0.001,
                  classification=False):
-
-        self.a = lossFunctionParam
-        self.__initialized = False  #set to true when done
-        self.maxIterations = 10
-
-        return None
+        self.__lossFunction = lossFunction
+        self.__lossFunctionParam = lossFunctionParam
+        self.__classification = classification
 
     def fit(self, X, y):
+        self.__initialized = False
 
-        X = np.vstack((X.T, np.ones(len(X))))
+        if self.__classification:
+            y = y.astype(numpy.int)
+            classes = list(set(y))
 
+            assert len(
+                classes
+            ) == 2, "This implementation of the Linear Regression classifier an handle only 2-class problems!"
+
+            self.__mapping = {}
+            self.__mapping[-1] = classes[0]
+            self.__mapping[+1] = classes[1]
+            y[y == classes[0]] = -1
+            y[y != -1] = +1
+
+        if len(y) < 2:
+            return
+
+        # special case: X is a 1-D numpy array
+        # mx = numpy.mean(X)
+        # my = numpy.mean(y)
+        # mxy = numpy.mean(X * y)
+        # a = (mxy - mx * my) / numpy.var(X)
+        # b = my - a * mx
+        # print("{0} * x + {1}".format(a, b))
+
+        X = numpy.vstack((X.T, numpy.ones(len(X))))
+        print(X)
+        print(X.shape)
         self.params = numpy.dot(
             numpy.dot(numpy.linalg.inv(numpy.dot(X, X.T)), X), y)
+        print(self.params.shape)
+        test1 = numpy.linalg.pinv(X)
+        test2 = numpy.linalg.pinv(X.T)
+        params = numpy.dot(test2,y)
 
+        if self.__lossFunction == 'huber':
+            a = self.__lossFunctionParam
 
-        while self.maxIterations:
+            res = scipy.optimize.minimize(
+                lambda x: self.huber_objfunc(X, y, x, a),
+                self.params,
+                method='BFGS',
+                jac=lambda x: self.huber_objfunc_derivative(X, y, x, a))
+            # print(res)
+            # print(res.x)
+            self.params = res.x
 
-            self.params -= 0.1 * self.huber_objfunc_derivative(X, y, self.params, self.a)
-
-            print(self.params)
-
-            self.maxIterations -= 1
-
-        # self.params = []
-
-        # intitial_params = np.zeros((X.shape[1],1))
-        # self.params.append(intitial_params)
-
-        # initial_bias = np.zeros(1)
-        # self.params.append(initial_bias)
-
-        # y_pred = (X @ self.params[0] +self.params[1]).reshape(y.shape[0],)
-
-        # y_pred = np.array([self.huber(x, self.a) for x in y-y_pred])
-
-        # y_pred = (self.params @ X)
-
-
+        self.__initialized = True
 
     def huber_objfunc(self, X, y, params, a):
-
         r = y - numpy.dot(params, X)
-
-        return r
+        return numpy.sum(self.huber(r, a))
 
     def huber_objfunc_derivative(self, X, y, params, a):
-
-        # r = y - numpy.dot(params, X)
-
-        r = self.huber_objfunc(X, y, params, a)
-
-        return -numpy.dot(X, np.array([self.huber_derivative(r_, a) for r_ in r]))
-
-        # common_term = y- (X @ params[0] + params[1]) 
-
-        # print(np.array([self.huber_derivative(r, a) for r in y-y_pred] * (y-y_pred)).reshape(y_pred.shape[0], 1).T @ X)
-
-        # dW = np.array([self.huber_derivative(r, a) for r in y-y_pred] * (y-y_pred)).reshape(y_pred.shape[0], 1).T @ X
-
-        # dB = np.sum(np.array([self.huber_derivative(r, a) for r in y-y_pred] * (y-y_pred)).reshape(y_pred.shape[0], 1).T * (-1))
-
-        # self.params[0] -= 0.1* dW
-        # self.params[1] -= 0.1* dB
-
-        # print(self.params[0])
-        # print(self.params[1])
-
-        # return None
+        r = y - numpy.dot(params, X)
+        return -numpy.dot(X, self.huber_derivative(r, a))
 
     def huber(self, r, a):
-
-        if abs(r) <= a:
-            return r*r
-
-        else:
-            return a*(2*abs(r) - a)
-
+        huber = numpy.zeros(len(r))
+        huber[abs(r) <= a] = r[abs(r) <= a]**2
+        huber[abs(r) > a] = a * (2 * abs(r[abs(r) > a]) - a)
+        return huber
 
     def huber_derivative(self, r, a):
-        
-        if abs(r) <= a:
-            return 2*r
-        elif r>a:
-            return 2*a
-        else:
-            return -2*a
-
+        derivative = numpy.zeros(len(r))
+        derivative[abs(r) <= a] = 2 * r[abs(r) <= a]
+        derivative[abs(r) > a] = 2 * a * numpy.sign(r[abs(r) > a])
+        return derivative
 
     def paint(self, qp, featurespace):
         if self.__initialized:
@@ -142,8 +117,16 @@ class LinearRegression(object):
             qp.drawLine(x1, y1, x2, y2)
 
     def predict(self, X):
-        return None
+        if not self.__initialized:
+            return None
 
+        # print(self.params)
+        a = self.params[0:-1]
+        a0 = self.params[-1]
+        Z = numpy.sign(numpy.sum(X * a, 1) + a0)
+        Z[Z > 0] = self.__mapping[+1]
+        Z[Z <= 0] = self.__mapping[-1]
+        return Z
 
 def main():
 
